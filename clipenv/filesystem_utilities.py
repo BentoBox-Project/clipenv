4d1ebeb05447ebeedcd018949804cc448ae4f76d
Valery Briz <valerybriz@gmail.com>
2019-10-06 21:40:31 -0500

    add method to check if the venv name is valid

--- clipenv/filesystem_utilities.py
+++ clipenv/filesystem_utilities.py
@@ -28,3 +28,7 @@ def filepath_from_option(option, existing_config_files, root=Path.home()):
 def _file_exists(root, config_file):
     filepath = os.path.join(root, config_file)
     return os.path.isfile(filepath)
+
+
+def is_valid_var_name(venv_name):
+    return False if ' ' in venv_name or '=' in venv_name else True
