abf6323916d0e223d1fcdcfe6b157135a2fd8491
Valery Briz <valerybriz@gmail.com>
2019-10-06 21:40:52 -0500

    add new method to list all the venvs in the file selected

--- clipenv/cli/commands.py
+++ clipenv/cli/commands.py
@@ -24,3 +24,24 @@ def add_env_var(variable_name, variable_value, profile_file_path):
         raise exceptions.ClipenvFileNotFoundError(e)
 
     return successfully_added
+
+
+def list_all_env_vars(profile_file_path):
+    """
+    receives a profile file path and lists all the containing venvs
+    previously stored in the file.
+    """
+    all_vars = ""
+    try:
+        # Check if the profile file exists
+        exists = os.path.isfile(profile_file_path)
+        if exists:
+            with open(profile_file_path, "r") as profile_file:
+                all_vars = profile_file.read()
+        else:
+            raise exceptions.ClipenvFileNotFoundError("File not Found")
+
+    except (exceptions.ClipenvIOError, Exception) as e:
+        raise exceptions.ClipenvFileNotFoundError(e)
+
+    return all_vars
